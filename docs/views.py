fd0b1427a8f12261b96e703d4df6420c24ab6805
lnxpy <lnxpylnxpy@gmail.com>
2020-03-28 19:35:47 +0430

    design sides finished successfully

--- docs/views.py
+++ docs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Documentation
 
-# Create your views here.
+def doc_list(requests):
+    docs = Documentation.objects.all()
+    context = {
+    'docs': docs,
+    'last_release': docs[len(docs)-1],
+    }
+    return render(requests, 'index.html', context=context)
+
+def doc(requests, version):
+    context = {
+        'document':get_object_or_404(Documentation, version=version)
+    }
+    return render(requests, 'document.html', context=context)
