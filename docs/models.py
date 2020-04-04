0d68670cbd6348399c9ff64ec503d8b8b71f8d13
lnxpy <lnxpylnxpy@gmail.com>
2020-04-04 18:33:51 +0430

    new fields added

--- docs/models.py
+++ docs/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+ALIGNMENT_CHOICES = [
+    ('left', 'Left (e.g. English)'),
+    ('right', 'Right (e.g. Persian, Arabic, Urdu, etc)'),
+]
+
+ALIGNMENT_HINT = """
+You may use Persian language.
+Don't forget to set the direction from the top corner of content editor.
+"""
+
+LANGUAGE_CHOICES = [
+    ('en', 'English'),
+    ('pe', 'Persian'),
+]
 
 class Documentation(models.Model):
     content = RichTextField()
-    version = models.CharField(max_length=10, default='v')
+    alignment = models.CharField(choices=ALIGNMENT_CHOICES, max_length=50, help_text=ALIGNMENT_HINT)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=5)
+    version = models.CharField(max_length=50)
 
     def __str__(self):
         return self.version
